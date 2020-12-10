24a278cc3e4bab53a4d4e0462a86b1820ad8ce7c
Aly Farag <afarag@Alys-MacBook-Pro.local>
2020-12-10 01:35:16 -0500

    mapped dataset to line chart; mapped form input to dataset

--- App.js
+++ App.js
@@ -4,40 +4,115 @@ import {
   Dimensions,
   View,
   SafeAreaView,
-  ScrollView,
+  // ScrollView,
   StyleSheet,
   Text,
   TextInput,
 } from "react-native";
+import moment from "moment";
 import { LineChart } from "react-native-chart-kit";
 import TrackIncome from "./trackIncome";
+
+//  Problem Solving: dataset needs to be in a state (variable)
 const App = () => {
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState("");
   const [total, setTotal] = useState(0);
+  const [labels, setLabels] = useState([]);
+  const [data, setData] = useState([
+    {
+      date: moment().format("LL"),
+      amount: 2000,
+    },
+    { date: moment().subtract(1, "days").format("LL"), amount: 2500 },
+    { date: moment().subtract(1, "days").format("LL"), amount: 3500 },
+    { date: moment().subtract(1, "days").format("LL"), amount: 3500 },
+    { date: moment().subtract(1, "days").format("LL"), amount: 3500 },
+    { date: moment().subtract(7, "days").format("LL"), amount: 3500 },
+    { date: moment().subtract(6, "days").format("LL"), amount: 3500 },
+    { date: moment().subtract(5, "days").format("LL"), amount: 3500 },
+    { date: moment().subtract(4, "days").format("LL"), amount: 3500 },
+    { date: moment().subtract(3, "days").format("LL"), amount: 4500 },
+    { date: moment().subtract(2, "days").format("LL"), amount: 5500 },
+    { date: moment().subtract(2, "days").format("LL"), amount: 5500 },
+  ]);
+  //  sort to dates, hook to form, connect to firebase 🔥
+  const [transformedData, setTransformedData] = useState([]);
+  useEffect(() => {
+    setTransformedData(transformData(groupBy(data, "date")));
+  }, [data]);
   const [gigs, setGigs] = useState([
     {
       description: "Freelance @ Upworks",
       amount: 499.99,
+      date: new Date(),
     },
   ]);
+  //  Expected Output: [ date1, date2, date3, date4, date5 ]
+  console.log("DEBUG 🔥", data);
 
-  useEffect(() => {
-    setTotal(gigs.reduce((total, gig) => total + Number(gig.amount), 0));
-  }, [gigs]);
+  const getDates = () => transformedData.map((pair) => pair.date);
+  console.log("The Dates ⏲", getDates());
+
+  const getAmounts = () => transformedData.map((pair) => pair.amount);
+  console.log("The Amounts 💰", getAmounts());
+
+  const groupBy = (array, key) =>
+    array.reduce((rv, x) => {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  console.log(
+    "The GROUPED values are 🎯",
+    Object.entries(groupBy(data, "date"))
+  );
+
+  const transformData = (groupedData) => {
+    const transformedArray = [];
+
+    Object.entries(groupedData).forEach((entry) => {
+      const total = entry[1].reduce((total, pair) => total + pair.amount, 0);
+      transformedArray.push({
+        date: moment(entry[0]).format("MM/DD"),
+        amount: total,
+      });
+    });
+    const sortedArray = transformedArray.sort((a, b) =>
+      moment(a["date"]).diff(moment(b["date"]))
+    );
+
+    console.log("Sorted:\n", sortedArray);
+    return sortedArray;
+    //["December 7, 2020", [{...}{...}]]
+  };
+  console.log(
+    "The TOTAL grouped values are 👽 ",
+    transformData(groupBy(data, "date"))
+  );
+
   const addGig = () => {
     setGigs([
       ...gigs,
       {
         description: description,
         amount: amount,
-        timestamp: new Date(),
+      },
+    ]);
+
+    setData([
+      ...data,
+      {
+        date: moment().format("LL"),
+        amount: Number(amount),
       },
     ]);
 
     setDescription("");
     setAmount("");
   };
+  useEffect(() => {
+    setTotal(gigs.reduce((total, gig) => total + Number(gig.amount), 0));
+  }, [gigs]);
 
   return (
     <SafeAreaView>
@@ -46,42 +121,27 @@ const App = () => {
           🚀🚀🚀 Track Income App 🚀🚀🚀
         </Text>
       </View>
-      <Text style={styles.subtitleTexts}>Total Income: ${total}</Text>
       <View>
         <Text>Your Income</Text>
         <LineChart
           data={{
-            labels: [
-              "Dec 5/20",
-              "Dec 6/20",
-              "Dec 7/20",
-              "Dec 8/20",
-              "Dec 9/20",
-              "Dec 10/20",
-            ],
+            labels: getDates(),
             datasets: [
               {
-                data: [
-                  total * 100,
-                  total * 200,
-                  total * 300,
-                  total * 400,
-                  total * 500,
-                  total * 600,
-                ],
+                data: getAmounts(),
               },
             ],
           }}
           width={Dimensions.get("window").width} // from react-native
           height={220}
           yAxisLabel="$"
-          yAxisSuffix="k"
+          //yAxisSuffix="k"
           yAxisInterval={1} // optional, defaults to 1
           chartConfig={{
             backgroundColor: "#e26a00",
-            backgroundGradientFrom: "#fb8c00",
-            backgroundGradientTo: "#ffa726",
-            decimalPlaces: 1, // optional, defaults to 2dp
+            backgroundGradientFrom: "green",
+            backgroundGradientTo: "green",
+            decimalPlaces: null, // optional, defaults to 2dp
             color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
             labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
             style: {
@@ -101,6 +161,9 @@ const App = () => {
           }}
         />
       </View>
+      <Text style={styles.subtitleTexts}>
+        Total Income: ${total.toFixed(2)}
+      </Text>
       {/* <Todo title="Take bio out" />
       <Todo title="Code React (Native), Flutter" />
       <Todo title="Pray, Read, Dua" /> */}
@@ -120,13 +183,13 @@ const App = () => {
       <Button
         disabled={!amount && !description}
         onPress={addGig}
-        style={styles.addgig__btn}
+        color="rgba(46, 204, 113,1.0)"
         title="Add Gig 🚀"
       />
       {gigs.map((gig) => (
         <View>
           <Text style={styles.subtitleTexts}>{gig.description}</Text>
-          <Text style={styles.subtitleTexts}>{gig.amount}</Text>
+          <Text style={styles.subtitleTexts}>${gig.amount}</Text>
         </View>
       ))}
     </SafeAreaView>
